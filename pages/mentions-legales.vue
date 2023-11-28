c5e1b3a7e36f13c0ac6ff2a3ed6e955b9f4c0597
Mihoko <melina.bouchelleghem@edu.univ-fcomte.fr>
2023-11-28 15:13:30 +0100

    feat(mentions) : connexion prismic à la page mentions

--- pages/mentions-legales.vue
+++ pages/mentions-legales.vue
@@ -1,7 +1,19 @@
-<script>
+<script setup>
+const { client } = usePrismic();
+const { data: mentions, error } = await useAsyncData('mentions', () => client.getSingle("mentions"));
+
+
+const props = defineProps({
+  title: Array,
+ 
+})
 </script>
 
 <template>
+
+<PrismicRichText :field="mentions.data.title "></PrismicRichText>
+<p>hello</p>
+
 </template>
 
 <style lang="scss" scoped>
