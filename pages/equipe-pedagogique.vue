e0731620a355c68d399d520a6d140e033f94af8b
Mihoko <melina.bouchelleghem@edu.univ-fcomte.fr>
2023-12-17 23:41:36 +0100

    feat(equipe) : mise en forme prismic de l'equipe pedagogique + responsive

---
+++ pages/equipe-pedagogique.vue
@@ -0,0 +1,69 @@
+<script setup>
+const { client } = usePrismic();
+  const { data: equipe, error } = await useAsyncData('equipe', () => client.getSingle("equipe"));
+
+
+const props = defineProps({
+  members: Array
+})
+
+
+  
+</script>
+
+<template>
+    <div class="container">
+        <PrismicRichText class="section1-title" :field="equipe.data.section1[0].titre"></PrismicRichText>
+        <div class="equipecard">
+            <equipecard :members="equipe.data.section2"></equipecard>
+        </div>
+       
+    </div>
+    
+<Footer></Footer>
+
+
+</template>
+
+<style lang="scss">
+.section1-title {
+    font-family: $font-redhat;
+    margin:rem(40) rem(0);
+    font-size: $size-32;
+    font-weight: 100;
+    text-transform: uppercase;
+    strong {
+    font-weight: 800;
+    }
+}
+
+@media (min-width: 768px) {
+.equipecard {
+    display:grid;
+    grid-template-columns: repeat(2, 1fr);
+    margin: rem(90) rem(0);
+ 
+}
+}
+
+@media (min-width: 1024px) {
+.equipecard {
+    display:grid;
+    grid-template-columns: repeat(3, 1fr);
+ 
+}
+}
+
+
+@media (min-width: 1440px) {
+.equipecard {
+    display:grid;
+    
+    grid-template-columns: repeat(4, 1fr);
+    margin: rem(100) rem(0);
+ 
+}
+}
+
+
+</style>
