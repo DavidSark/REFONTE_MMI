4a693b9665e033b04fbd339e21f380112898d02b
Mihoko <melina.bouchelleghem@edu.univ-fcomte.fr>
2023-12-22 01:30:06 +0100

    feat(departement) : finalisation responsive section2&3

--- pages/contact.vue
+++ pages/contact.vue
@@ -16,20 +16,7 @@ const { data: contact, error } = await useAsyncData('contact', () => client.getS
                      />
         <Button class="sectionB-sideleft__btn" size="small">En savoir plus</Button>
         </div>
-        <div class="section1-contact__info">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2695.714464850608!2d6.802704075879118!3d47.495475595645296!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4792172655793f9f%3A0x1b3b0f76560084ad!2sD%C3%A9partement%20MMI%20de%20Montb%C3%A9liard!5e0!3m2!1sfr!2sfr!4v1703111614273!5m2!1sfr!2sfr" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe> 
-            <h5><span class="circle"></span>Où nous retrouver</h5>
-            <h6>4 place Tharradin</h6>
-            <h6>25200 Montbéliard</h6>
-            <h6>tél : +33 (0)3 81 99 47 34</h6>
-            <h6>Mail : [...]</h6>
-            <div class="section1-contact__info-socials">
-                <a href="https://www.facebook.com/mmimontbeliard" target="_blank"><img src="/icons/icon-facebook.svg" alt="facebook"></a>
-        <a href="https://www.instagram.com/mmi_montbeliard" target="_blank"><img src="/icons/icon-instagram.svg" alt="instagram"></a>
-        <a href="https://www.linkedin.com/school/mmimontbeliard" target="_blank"><img src="/icons/icon-linkedin.svg" alt="linkedin"></a>
-
-            </div>
-        </div>
+        <Cardgooglemap></Cardgooglemap>
     </div>
     <div class="section2-contact">
         <img src="/image9.webp" alt="">
@@ -59,39 +46,6 @@ const { data: contact, error } = await useAsyncData('contact', () => client.getS
     gap:rem(40);
     display: flex;
     flex-direction: column;
-    &__info {
-        iframe{
-            width: 100%;
-        }
-    &-socials {
-        display: flex;
-        justify-content: flex-end;
-        gap:rem(20);
-        img {
-            width: rem(30);
-        }
-    }
-    h6{
-        font-family: $font-poppins;
-        text-transform: uppercase;
-        font-weight: 300;
-        font-size: $size-13;
-        margin-bottom: rem(6)
-    }
-    h5 {
-        margin-bottom: rem(15);
-        margin-top: rem(20);
-        font-family: $font-redhat;
-        font-size: $size-20;
-        text-transform: uppercase;
-        font-weight: 700;
-    }
-    .circle {
-        width: 50px;
-        height: 50px;
-        background-color: red;
-    }
-    }
 }
 
 .section2-contact {
@@ -139,11 +93,6 @@ const { data: contact, error } = await useAsyncData('contact', () => client.getS
 @media (min-width : 768px) {
     .section1-contact {
         padding:rem(30) rem(40);
-    &__info {
-    h6 {
-        font-size:$size-16
-    }
-    }
     }
 
     .section2-contact {
@@ -186,12 +135,6 @@ const { data: contact, error } = await useAsyncData('contact', () => client.getS
         grid-template-columns: repeat(2,1fr);
         align-items: center;
         gap: rem(150);
-        &__info {
-            &-socials {
-                justify-content: flex-start;
-                margin:rem(20) rem(0);
-            }
-        }
         
     }
     .section3-contact {
--- pages/departement.vue
+++ pages/departement.vue
@@ -1,8 +1,211 @@
-<script>
-</script>
+<script setup>
+const { client } = usePrismic();
+const { data: departement, error } = await useAsyncData('departement', () => client.getSingle("departement"));
 
+  const props = defineProps({
+  
+});
+</script>
 <template>
+    <div class="section1-departement">
+        <div class="section1-departement__content">
+     <borderSection :title="departement.data.section1[0].title"
+                  :subtitle="departement.data.section1[0].subtitle"
+                     />
+        </div>
+        <div class="section1-departement__card">
+    <cardgooglemap></cardgooglemap>
+        </div>
+     
+    </div>
+    <img src="/image-video-dep.webp" alt="">
+
+    <div class="section2-departement">
+        <PrismicRichText class="section2-departement__title" :field="departement.data.actualites[0].title"></PrismicRichText>
+
+        <div class="section2-departement__card">
+        <div class="section2-departement__card-1">
+            <img src="/image-dep-card1.webp" alt="">
+            <h6><span>Bienvenue aux </span>étudiants 2023</h6>
+            <p>C’est partie pour une nouvelle édition de la journée. <span>Bienvenue aux étudiants</span> où les différentes promos de MMI ont pu se réunir pour participer à un jeu de piste !</p>
+            <Button class="section2-departement__card__btn" size="small" >En savoir plus</Button>
+        </div>
+        <div class="section2-departement__card-2">
+            <img src="/image-dep-card2.webp" alt="">
+            <h6><span>offf </span>barcelona 2023</h6>
+            <p>Certains de nos étudiants ont pu partir en voyage à Barcelone en participant au <span>Festival du Design “OFFF”.</span></p>
+            <Button class="section2-departement__card__btn" size="small" >En savoir plus</Button>
+        </div>
+        <div class="section2-departement__card-3">
+            <img src="/image-dep-card3.webp" alt="">
+            <h6><span>rentrée </span>2023</h6>
+            <p>C’est partie pour une <span>nouvelle année</span>, pour nos étudiants de MMI. Au programme quelques jours de découverte pour nos nouveaux étudiants.</p>
+            <Button class="section2-departement__card__btn" size="small" >En savoir plus</Button>
+        </div>
+    </div>
+    </div>
+    <div class="section3-departement">
+        <div class="section3-departement__sideright">
+            <borderSection :title="departement.data.section3[0].title"
+                  :subtitle="departement.data.section3[0].subtitle"
+                  color="red" borderPosition="right"
+                     />
+  
+        </div>
+        <img src="/image-dep-bureau.webp" alt="">
+    </div>
+
+
 </template>
 
-<style lang="scss" scoped>
+<style lang="scss">
+
+.section3-departement {
+    display: flex;
+    flex-direction: column;
+    &__sideright {
+        padding:rem(30) rem(20);
+    }
+
+}
+.section2-departement {
+    padding:rem(30) rem(20);
+    &__card {
+        display: flex;
+        flex-direction: column;
+        gap:rem(60);
+        h6 {
+                margin:rem(10) rem(0);
+                text-transform: uppercase;
+                font-family: $font-redhat;
+                font-size:$size-14;
+                span {
+                    font-weight: 700;
+                }
+              
+            }
+            p {
+                    font-family: $font-poppins;
+                    font-size: $size-13;
+                    font-weight: 300;
+                    line-height: normal;
+                    span {
+                    font-weight: 700;
+                    color:$green;
+                }
+                }
+    
+  
+            &__btn {
+                    cursor: pointer;
+                    margin-top:rem(10)
+                }
+        
+        &-2 {
+            p {
+                span{
+                    color:$blue;
+                }
+            }
+
+        }
+        &-3 {
+            p {
+                span{
+                    color:$red;
+                }
+            }
+
+        }
+
+    }
+   
+    &__title {
+        
+        font-family: $font-redhat;
+    margin:rem(40) rem(0);
+    font-size: $size-32;
+    font-weight: 100;
+    text-transform: uppercase;
+    strong {
+    font-weight: 800;
+    }
+    }
+}
+.section1-departement {
+    padding:rem(30) rem(20);
+    display: flex;
+    flex-direction: column;
+    gap:rem(40);
+}
+
+img {
+    width: 100%;
+}
+
+@media (min-width : 768px) {
+
+    .section3-departement {
+        padding:rem(30) rem(40);
+        display:flex ;
+ 
+    }
+
+
+    .section1-departement {
+        padding:rem(30) rem(40);
+    }
+
+    .section2-departement {
+        padding:rem(30) rem(40);
+        &__card{
+            display: grid;
+            grid-template-columns: repeat(2, 1fr);
+           h6 {
+            font-size:$size-16;
+           }
+           p {
+            font-size: $size-16;
+           }
+        }
+    }
+}
+
+@media (min-width: 1024px ) {
+    .section2-departement{
+        &__title {
+            font-size: $size-45;
+        }
+    }
+}
+
+@media (min-width: 1440px) {
+
+    .section1-departement{
+        padding:rem(120) rem(75);
+        display: grid;
+        grid-template-columns: repeat(2,1fr);
+        align-items: center;
+        gap: rem(150);     
+    }
+
+    .section2-departement {
+        padding:rem(110) rem(75);
+        &__title {
+            font-size: $size-90;
+        }
+        &__card{
+            margin:rem(100) rem(0);
+            gap: rem(100);
+            display: grid;
+            grid-template-columns: repeat(3, 1fr);
+           h6 {
+            font-size:$size-16;
+           }
+           p {
+            font-size: $size-16;
+           }
+        }
+    }
+}
 </style>
