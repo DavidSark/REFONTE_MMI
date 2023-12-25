b2033381c64e4a25ff2b2541ba6d7a134dd33cc0
DavidSark <david.sarkissian@edu.univ-fcomte.fr>
2023-12-25 19:05:05 +0100

    feat(index) : tagline de l'index + responsive

--- pages/index.vue
+++ pages/index.vue
@@ -10,49 +10,80 @@ const { data: contact } = await useAsyncData('contact', () => client.getSingle("
 
 
 <template>
-    <div class="container_index">
-    <Header :elements="home.data.header"></Header>
+    <div class="container__index">
 
-    <div class="container_index-heroM">
-      <HeroM></HeroM>
-    </div>
-   
-    <div class="container_index-heroT">
-      <HeroT></HeroT>
-    </div>
-    <div class="container_index-heroC">
-      <HeroC></HeroC>
-    </div>
+      <Header :elements="home.data.header"></Header>
+
+      <div class="container__index-heroM">
+        <HeroM></HeroM>
+      </div>
+    
+      <div class="container__index-heroT">
+        <HeroT></HeroT>
+      </div>
+      <div class="container__index-heroC">
+        <HeroC></HeroC>
+      </div> 
+
+      <div class="section1-index">
+        <div class="section1-index__tagline">
+         <PrismicRichText :field="home.data.index_tagline"></PrismicRichText>
+        </div>
+
+        
+
+      </div>
 
    <Footer></Footer>
-
-  
   </div>
 </template>
 
 
-<style lang="scss" scoped>
-.container_index{
+<style lang="scss" >
+.container__index{
+  //pour le hero
   &-heroT{
     display: none;
   }
   &-heroC{
     display: none;
   }
+
+  .section1-index{
+    padding: rem(50) rem(20);
+   
+    
+    &__tagline{
+      font-family: $font-poppins;
+      font-size: $size-24;
+      text-transform: uppercase;
+      strong{
+       font-weight: 700;
+      }
+    }
+  }
+
 }
 
 @media screen and (min-width:768px) {
-  .container_index{
+  .container__index{
     &-heroM{
       display: none;
     }
     &-heroT{
       display: block;
     }
+
+    .section1-index{
+      padding: rem(30) rem(40);
+    &__tagline{
+      font-size: $size-32;
+    }
+   }
   }
 }
 @media screen and (min-width:1024px) {
-  .container_index{
+  .container__index{
     &-heroM{
       display: none;
     }
@@ -62,6 +93,24 @@ const { data: contact } = await useAsyncData('contact', () => client.getSingle("
     &-heroC{
       display: block;
     }
+    .section1-index{
+      padding: rem(30) rem(40);
+    &__tagline{
+      font-size: 4vw;
+ 
+    }
+   }
+  }
+}
+
+@media screen and (min-width:1440px) {
+  .container__index{
+    .section1-index{
+      padding: rem(170) rem(75);
+    &__tagline{
+      max-width: 1400px;
+    }
+   }
   }
 }
 </style>
