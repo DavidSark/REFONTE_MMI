78170d35b31a1505060dc3600a719698a24c296c
DavidSark <david.sarkissian@edu.univ-fcomte.fr>
2023-12-25 18:19:48 +0100

    feat(index) : ajout du hero dans l'index

--- pages/index.vue
+++ pages/index.vue
@@ -10,13 +10,58 @@ const { data: contact } = await useAsyncData('contact', () => client.getSingle("
 
 
 <template>
-    <div class="container">
-        <Header :elements="home.data.header"></Header>
-        <Footer></Footer>
-    </div>  
+    <div class="container_index">
+    <Header :elements="home.data.header"></Header>
+
+    <div class="container_index-heroM">
+      <HeroM></HeroM>
+    </div>
+   
+    <div class="container_index-heroT">
+      <HeroT></HeroT>
+    </div>
+    <div class="container_index-heroC">
+      <HeroC></HeroC>
+    </div>
+
+   <Footer></Footer>
+
+  
+  </div>
 </template>
 
 
 <style lang="scss" scoped>
+.container_index{
+  &-heroT{
+    display: none;
+  }
+  &-heroC{
+    display: none;
+  }
+}
 
+@media screen and (min-width:768px) {
+  .container_index{
+    &-heroM{
+      display: none;
+    }
+    &-heroT{
+      display: block;
+    }
+  }
+}
+@media screen and (min-width:1024px) {
+  .container_index{
+    &-heroM{
+      display: none;
+    }
+    &-heroT{
+      display: none;
+    }
+    &-heroC{
+      display: block;
+    }
+  }
+}
 </style>
