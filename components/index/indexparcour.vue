ead919e9c7f1d1cdd6a94473e74b84ae347844f0
DavidSark <david.sarkissian@edu.univ-fcomte.fr>
2023-12-27 01:22:36 +0100

    feat(index) : maj du code de la section parcours dans l'index (responsive et rond de couleur)

--- components/index/indexparcour.vue
+++ components/index/indexparcour.vue
@@ -6,78 +6,131 @@
 
 <template>
     <div class="section-indexparcours">
+        <div class="section-indexparcours__line"></div>
         <div class="section-indexparcours__design">
-            <h2>design</h2>
-            <div></div>
+            <h2>design<span>.</span></h2>
+            <!-- <div></div> -->
             <img src="/arrow.svg" alt="">
         </div>
+        <div class="section-indexparcours__line"></div>
         <div class="section-indexparcours__com">
-            <h2>communication</h2>
-            <div></div>
+            <h2>communication<span>.</span></h2>
+            <!-- <div></div> -->
             <img src="/arrow.svg" alt="">
         </div>
-        
+        <div class="section-indexparcours__line"></div>
         <div class="section-indexparcours__dev">
-            <h2>développement web</h2>
-            <div></div>
+            <h2>développement web<span>.</span></h2>
+            <!-- <div></div> -->
             <img src="/arrow.svg" alt="">
         </div>
+        <div class="section-indexparcours__line"></div>
     </div>
 </template>
 
 
 <style lang="scss" >
 .section-indexparcours{
+    margin: rem(50) rem(0);
     font-family: $font-poppins;
     font-weight: 700;
     font-size: 7.5vw;
     text-transform: uppercase;
     display: flex;
     flex-direction: column;
-    gap: 2.5vw;
-    letter-spacing: rem(-3);
+    gap: 1vw;
+    letter-spacing: rem(-2);
+
+    &__line{
+        padding: initial;
+        height: rem(1); 
+        width: 100%;
+        background-color: $white-soft;
+    }
     img{
-        margin:0 0 1vw 1.4vw;
-        width: 2.5vw;
+        margin:0 0 1vw 1vw;
+        // width: 2.5vw;
+        width: rem(12)
     }
    &__design{
+    padding: rem(0) rem(20);
     display: flex;
     align-items: flex-end;
-    div{
-        margin-bottom: 0.95vw;
-        width: 1vw;
-        height: 1vw;
-        background-color: $blue;
-        border-radius: rem(10);
+    // div{
+    //     margin: 0 0  1.2vw rem(3) ;
+    //     width: rem(5);
+    //     height: rem(5);
+    //     background-color: $blue;
+    //     border-radius: rem(10);
+    // }
+    span{
+        color: $blue;
     }
    }
    &__com{
     display: flex;
     align-items: flex-end;
-    div{
-        margin-bottom: 0.95vw;
-        width: 1vw;
-        height: 1vw;
-        background-color: $red;
-        border-radius: rem(10);
+    padding: rem(0) rem(20);
+    // div{
+    //     margin: 0 0  1.2vw rem(3) ;
+    //     width: rem(5);
+    //     height: rem(5);
+    //     background-color: $red;
+    //     border-radius: rem(10);
+    span{
+        color: $red;
     }
+    // }
    }
    &__dev{
     display: flex;
     align-items: flex-end;
-    div{
-        margin-bottom: 0.95vw;
-        width: 1vw;
-        height: 1vw;
-        background-color: $green;
-        border-radius: rem(10);
+    padding: rem(0) rem(20);
+    span{
+        color: $green;
     }
+    // div{
+    //     margin: 0 0 1.2vw rem(3) ;
+    //     width: rem(5);
+    //     height: rem(5);
+    //     background-color: $green;
+    //     border-radius: rem(10);
+    // }
    }
+}
+
+
+
+@media  screen and (min-width: 768px) {
+    .section-indexparcours{
+    img{
+        width: 3vw;
+    }
+   &__design{
+    padding: rem(0) rem(40);
+   }
+   &__com{
+    padding: rem(0) rem(40);
+   }
+   &__dev{
+    padding: rem(0) rem(40);
+   }
+}
 }
 
 @media screen and (min-width:1440px) {
     .section-indexparcours{
-        font-size: rem(133);
+        font-size: $size-100;
+        &__design{
+            padding: rem(0) rem(75);
+        }
+        &__com{
+            padding: rem(0) rem(75);
+        }
+        &__dev{
+            padding: rem(0) rem(75);
+        }
     }
+   
 }
 </style>
