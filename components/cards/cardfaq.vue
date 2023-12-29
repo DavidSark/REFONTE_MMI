58da3964d1223dea4156202b487f78dbc7164db6
Mihoko <melina.bouchelleghem@edu.univ-fcomte.fr>
2023-12-29 16:27:51 +0100

    fix(contenu) : contenu à jours des faq

--- components/cards/cardfaq.vue
+++ components/cards/cardfaq.vue
@@ -24,51 +24,51 @@ const { data: faq, error } = await useAsyncData('faq', () => client.getSingle("f
                 </div> -->
                 <div class="tab">
                     <input type="checkbox" id="chck1">
-                    <label class="tab-label" for="chck1">Quand se déroule les portes ouvertes ?</label>
+                    <label class="tab-label" for="chck1">Comment s'inscrire à la formation pour les nouveaux étudiants? </label>
                     <div class="tab-content">
-                        L'ensemble de ce site relève de la législation française et internationale sur le droit d'auteur et la propriété intellectuelle. 
+                        Au BUT MMI de Montbéliard, l'inscription se fait via Parcoursup, où les candidats peuvent mettre en avant leurs compétences pour être potentiellement sélectionnés. 
                     </div>
                 </div>
                 <div class="tab">
                     <input type="checkbox" id="chck2">
-                    <label class="tab-label" for="chck2">Quand se déroule les portes ouvertes ?</label>
+                    <label class="tab-label" for="chck2">Quels sont les parcours offerts dans la formation ?</label>
                     <div class="tab-content">
-                        L'ensemble de ce site relève de la législation française et internationale sur le droit d'auteur et la propriété intellectuelle. 
+                        Les parcours sont Communication, Design et Développement Web, chacun ciblant des compétences spécifiques liées aux métiers du multimédia. 
                     </div>
                 </div>
                 <div class="tab">
                     <input type="checkbox" id="chck3">
                     <label class="tab-label" for="chck3">Quand se déroule les portes ouvertes ?</label>
                     <div class="tab-content">
-                        L'ensemble de ce site relève de la législation française et internationale sur le droit d'auteur et la propriété intellectuelle. 
+                        La journée portes ouvertes vise à permettre aux visiteurs de découvrir les locaux, d'échanger avec les étudiants et les enseignants pour mieux appréhender le programme. Cette année, les portes ouvertes se dérouleront le 27 janvier 2024. 
                     </div>
                 </div>
                 <div class="tab">
                     <input type="checkbox" id="chck4">
-                    <label class="tab-label" for="chck4">Quand se déroule les portes ouvertes ?</label>
+                    <label class="tab-label" for="chck4">Options d'alternance disponibles pour les étudiants ?</label>
                     <div class="tab-content">
-                        L'ensemble de ce site relève de la législation française et internationale sur le droit d'auteur et la propriété intellectuelle. 
+                        Les étudiants peuvent signer un contrat d'alternance de la deuxième année (ou troisième) jusqu'à la fin de la formation, leur permettant de combiner études et expérience en entreprise.
                     </div>
                 </div>
                 <div class="tab">
                     <input type="checkbox" id="chck5">
-                    <label class="tab-label" for="chck5">Quand se déroule les portes ouvertes ?</label>
+                    <label class="tab-label" for="chck5">Quelle est la durée des stages obligatoires ?</label>
                     <div class="tab-content">
-                        L'ensemble de ce site relève de la législation française et internationale sur le droit d'auteur et la propriété intellectuelle. 
+                        Les stages obligatoires durent 10 semaines en 2ème année et 16 semaines en 3ème année, se déroulant généralement de mars à juin pour la 2ème année et février à juin pour la 3ème année.
                     </div>
                 </div>
                 <div class="tab">
                     <input type="checkbox" id="chck6">
-                    <label class="tab-label" for="chck6">Quand se déroule les portes ouvertes ?</label>
+                    <label class="tab-label" for="chck6">Est-il possible d'étudier à l'internationale ?</label>
                     <div class="tab-content">
-                        L'ensemble de ce site relève de la législation française et internationale sur le droit d'auteur et la propriété intellectuelle. 
+                        Les étudiants ont l'opportunité de réaliser des stages ou des alternances à l'étranger, offrant une chance de renforcer leurs compétences linguistiques et techniques dans le domaine du numérique. Ils peuvent également passer leur 1er semestre de 2ème année avec deux CEGEPs canadiens partenaires.
                     </div>
                 </div>
                 <div class="tab">
                     <input type="checkbox" id="chck7">
-                    <label class="tab-label" for="chck7">Quand se déroule les portes ouvertes ?</label>
+                    <label class="tab-label" for="chck7">Peut-on intégrer la formation sans connaissances en programmation ?</label>
                     <div class="tab-content">
-                        L'ensemble de ce site relève de la législation française et internationale sur le droit d'auteur et la propriété intellectuelle. 
+                        Oui, la formation MMI ne requiert pas de connaissances préalables en codage. Vous apprendrez à zéro dès la 1ère année de formation.
                     </div>
                 </div>
             </div>
