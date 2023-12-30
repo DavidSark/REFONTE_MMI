b86b5b1cbe4c8e7c2c9cb4bd203d5bb8e8230934
DavidSark <david.sarkissian@edu.univ-fcomte.fr>
2023-12-30 03:32:09 +0100

    feat(index) : responsive image

--- components/index/indexespacepro.vue
+++ components/index/indexespacepro.vue
@@ -61,20 +61,77 @@
         align-items: center;
         justify-content: flex-end;
         margin-top: rem(20);
+        overflow: hidden;
+  
         img{
         width: rem(187);
         height: rem(150);
         object-fit: cover;
+        transform: scale(1.2);
+
         }
     &-line{
         position: absolute;
-        bottom: rem(85);
+        bottom: 50%;
         width: 100%;
         height: rem(1);
         background-color: $white-soft;
         z-index: -3;
 
     }
+    }
+}
+
+@media screen and (min-width:425px) {
+    .section-espacepro{
+        &__image{
+            margin-top: 0;
+       
+            img{
+                width: 50%;
+                height: auto;
+                max-width: rem(412);
+                max-height: rem(275);
+            }
+        }
+    }
+}
+@media screen and (min-width:768px) {
+    .section-espacepro{
+        &__image{
+            margin-top: 0;
+            img{
+                width: 60%;
+                // object-position: 0 30%;
+                // width: 921px;
+                // height: 410px;
+            }
+        }
+    }
+}
+@media screen and (min-width:1024px) {
+    .section-espacepro{
+        &__image{
+            margin-top: 0;
+            img{
+                max-width: rem(560);
+                max-height: rem(360);
+            }
+        }
+    }
+}
+@media screen and (min-width:1440px) {
+    .section-espacepro{
+        &__image{
+            margin-top: 0;
+            img{
+                max-width: rem(921);
+                max-height: rem(410);
+                transform: initial;
+                // width: 921px;
+                // height: 410px;
+            }
+        }
     }
 }
 </style>
