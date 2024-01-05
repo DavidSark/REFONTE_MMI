731059885f5fd6e89e2837ba2df9451527ecae5c
Mihoko <melina.bouchelleghem@edu.univ-fcomte.fr>
2024-01-05 01:24:50 +0100

    feat(prismic) : modif scss

--- components/global/footer.vue
+++ components/global/footer.vue
@@ -10,42 +10,51 @@
             <h2>Métiers du multimédia et de l'internet</h2>
         </div>
         <div class="footer-links">
-            <RouterLink to="/mentions-legales"><p>Mentions légales</p></RouterLink>
+            <RouterLink to="/mentions-legales">
+                <p>Mentions légales</p>
+            </RouterLink>
             <div></div>
-            <RouterLink to="/confidentialite"><p>Confidentialité</p></RouterLink>
+            <RouterLink to="/confidentialite">
+                <p>Confidentialité</p>
+            </RouterLink>
         </div>
         <div class="footer-icons">
-        <a href="https://www.facebook.com/mmimontbeliard" target="_blank"><img src="/icons/icon-facebook.svg" alt="facebook"></a>
-        <a href="https://www.instagram.com/mmi_montbeliard" target="_blank"><img src="/icons/icon-instagram.svg" alt="instagram"></a>
-        <a href="https://www.linkedin.com/school/mmimontbeliard" target="_blank"><img src="/icons/icon-linkedin.svg" alt="linkedin"></a>
+            <a href="https://www.facebook.com/mmimontbeliard" target="_blank"><img src="/icons/icon-facebook.svg"
+                    alt="facebook"></a>
+            <a href="https://www.instagram.com/mmi_montbeliard" target="_blank"><img src="/icons/icon-instagram.svg"
+                    alt="instagram"></a>
+            <a href="https://www.linkedin.com/school/mmimontbeliard" target="_blank"><img src="/icons/icon-linkedin.svg"
+                    alt="linkedin"></a>
         </div>
     </div>
-
 </template>
 
 <style lang="scss" scoped>
 .footer {
     padding: rem(25) 0;
-    border-top :2px solid $white-soft;
+    border-top: 2px solid $white-soft;
+
     &-links {
         font-weight: 300;
         font-family: $font-poppins;
         font-size: $size-12;
         text-transform: uppercase;
         display: flex;
-        gap:rem(10);
+        gap: rem(10);
         justify-content: center;
-        margin-bottom:rem(30);
+        margin-bottom: rem(30);
+
         a {
-        text-decoration-line: underline;
-        color: $gray-soft;
+            text-decoration-line: underline;
+            color: $gray-soft;
         }
+
         :nth-child(2) {
             background: $gray-soft;
             width: rem(1);
             height: rem(13);
-          }
-       
+        }
+
     }
 
     &-subtitles {
@@ -56,26 +65,35 @@
         display: flex;
         flex-direction: column;
         align-items: center;
-        gap:rem(5);
+        gap: rem(5);
         margin-bottom: rem(20);
-        
-        
+
+
         img {
             width: rem(37);
             margin-bottom: rem(10);
+
         }
+
         :nth-child(3) {
             font-family: $font-poppins;
             font-weight: 300;
             font-size: $size-13;
         }
     }
+
     &-icons {
         display: flex;
         justify-content: center;
         gap: rem(20);
-        
+
         img {
+            transition: transform 0.3s ease-in-out;
+
+            &:hover {
+                transform: scale(1.2);
+            }
+
             width: rem(30);
         }
     }
@@ -83,30 +101,31 @@
 
 //media queries
 @media (min-width: 1440px) {
-.footer {
-    &-links {
-        font-size: $size-13;
-        margin-bottom:rem(30);      
+    .footer {
+        &-links {
+            font-size: $size-13;
+            margin-bottom: rem(30);
+        }
+
+        &-subtitles {
+            font-size: $size-26;
+
+            img {
+                width: rem(45);
+                margin-bottom: rem(15);
+            }
+
+            :nth-child(3) {
+                font-size: $size-16;
+            }
+        }
+
+        &-icons {
+            img {
+                width: rem(30);
+            }
+        }
+
     }
 
-    &-subtitles {
-        font-size: $size-26;
-        img {
-            width: rem(45);
-            margin-bottom: rem(15);
-        }
-        :nth-child(3) {
-            font-size: $size-16;
-        }
-    }
-    &-icons { 
-        img {
-            width: rem(30);
-        }
-    }
-
-}
-
-}
-
-</style>
+}</style>
--- components/global/hero/heroC.vue
+++ components/global/hero/heroC.vue
@@ -5,138 +5,165 @@ const { data: home, error } = await useAsyncData('home', () => client.getSingle(
 </script>
 
 <template>
-    <div class="container-hero">
-      <div class="container-hero__left">
-        <div class="container-hero__left-title">
-            <h1>Mmi Montbéliard</h1>
-            <h2>Métiers du Multimédia et de l'internet</h2>
-        </div>
-       
-        <div class="container-hero__left-buttons">
-          <RouterLink to="/candidater">
-          <Button size="small" bgColor="bgBlue" borderColor="borderBlue" color="white">Candidater</Button>
-          </RouterLink>
-          <RouterLink to="/formation">
-          <Button size="small" bgColor="bgTransparent" >Formation</Button></RouterLink>
+  <div class="container-hero">
+    <div class="container-hero__left">
 
-
-        </div>
-        <div class="container-hero__left-social">
-            <p>retrouvez-nous sur nos réseaux sociaux !</p>
-            <div class="container-hero__left-social-line"></div>
-            <div class="container-hero__left-social-icons">
-              <a href="https://www.facebook.com/mmimontbeliard" target="_blank"><img src="/icons/icon-facebook.svg" alt="icone facebook"></a>
-              <a href="https://www.instagram.com/mmi_montbeliard/" target="_blank"><img src="/icons/icon-instagram.svg" alt="icone instagram"></a>
-              <a href="https://www.linkedin.com/school/mmimontbeliard" target="_blank"><img src="/icons/icon-linkedin.svg" alt="icone linkedin"></a>
-            </div>
-          </div>
+      <div class="container-hero__left-title">
+        <h1>Mmi Montbéliard</h1>
+        <h2>Métiers du Multimédia et de l'internet</h2>
       </div>
-     
-      <div class="container-hero__right">
-        <img src="/image1.webp" alt="Cours en MMI">      
-        <img src="/image3.webp" alt="Cours en MMI">      
+
+      <div class="container-hero__left-buttons">
+        <RouterLink to="/candidater">
+          <Button size="small" bgColor="bgBlue" borderColor="borderBlue" color="white">Candidater</Button>
+        </RouterLink>
+        <RouterLink to="/formation">
+          <Button size="small" bgColor="bgTransparent">Formation</Button>
+        </RouterLink>
+      </div>
+
+
+
+      <div class="container-hero__left-social">
+        <p>retrouvez-nous sur nos réseaux sociaux !</p>
+        <div class="container-hero__left-social-line"></div>
+        <div class="container-hero__left-social-icons">
+          <a href="https://www.facebook.com/mmimontbeliard" target="_blank"><img src="/icons/icon-facebook.svg"
+              alt="icone facebook"></a>
+          <a href="https://www.instagram.com/mmi_montbeliard/" target="_blank"><img src="/icons/icon-instagram.svg"
+              alt="icone instagram"></a>
+          <a href="https://www.linkedin.com/school/mmimontbeliard" target="_blank"><img src="/icons/icon-linkedin.svg"
+              alt="icone linkedin"></a>
+        </div>
       </div>
     </div>
+
+    <div class="container-hero__right">
+      <img src="/image1.webp" alt="Cours en MMI">
+      <img src="/image3.webp" alt="Cours en MMI">
+    </div>
+  </div>
 </template>
 
- <style lang="scss" scoped>
+<style lang="scss" scoped>
 .container-hero {
-    font-family: $font-poppins;
+  font-family: $font-poppins;
+  border-bottom: 2px solid $white-soft;
 
+  display: flex;
+  height: 100vh;
+
+  &__left {
+
+    width: 50%;
     display: flex;
-    height: 100vh;
-    &__left{
-        width: 50%;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        &-title{
-        padding: rem(0) rem(35) rem(35) rem(35);
-        :nth-child(1){
-          font-family: $font-redhat;
-          letter-spacing: -6.38px;
-            font-size: $size-60; 
-            font-weight: 700;
-            text-transform: uppercase;
-            display: table-caption;
-        }
-        :nth-child(2){
-            font-size: $size-16;    
-            letter-spacing: rem(3);
-            color: $gray-soft;   
-            text-transform: uppercase;
-        }
-        }
-        &-buttons{
-            padding: rem(0) rem(35) rem(35) rem(35);
-            display: flex;
-            gap: rem(20);
-        }
-        &-social{
-            
-            text-transform: uppercase;
-            display: flex;
-            flex-direction: column;
-            gap: rem(10);
-            padding: rem(0) rem(35) rem(35) rem(35);
-            font-size: $size-13;
-            font-weight: 300;
-            color: $gray-soft;
-            &-icons{
-             display: flex;
-             align-items: flex-end;
-             gap: rem(20);
-             img{
-                width: rem(20);
-                height: rem(20);
-             }
-            }
-        } 
+    justify-content: center;
+    flex-direction: column;
+
+    &-title {
+      padding: rem(0) rem(35) rem(35) rem(35);
+
+      :nth-child(1) {
+        font-family: $font-redhat;
+        letter-spacing: -6.38px;
+        font-size: $size-60;
+        font-weight: 700;
+        text-transform: uppercase;
+        display: table-caption;
+      }
+
+      :nth-child(2) {
+        font-size: $size-16;
+        letter-spacing: rem(3);
+        color: $gray-soft;
+        text-transform: uppercase;
+      }
     }
 
-    &__right{
-        width: 50%;
+    &-buttons {
+      padding: rem(0) rem(35) rem(35) rem(35);
+      display: flex;
+      gap: rem(20);
+    }
+
+    &-social {
+
+      text-transform: uppercase;
+      display: flex;
+      flex-direction: column;
+
+
+      gap: rem(10);
+      padding: rem(0) rem(35) rem(35) rem(35);
+      font-size: $size-13;
+      font-weight: 300;
+      color: $gray-soft;
+
+      &-icons {
         display: flex;
-        gap:rem(5);
-        overflow: hidden;
-        img{
-            width: 50%;
-            object-fit: cover;
-            object-position: 65% 50%;
-            filter: grayscale(1);
-        }           
+        align-items: flex-end;
+        gap: rem(20);
+
+        img {
+          width: rem(20);
+          height: rem(20);
+          transition: transform 0.3s ease-in-out;
+
+          &:hover {
+            transform: scale(1.3);
+          }
+
+        }
+      }
     }
   }
 
+  &__right {
+    width: 50%;
+    display: flex;
+    gap: rem(5);
+    overflow: hidden;
 
-  @media (min-width: 1024px) {
-    .container-hero {
-    &__left{
-        &-title{
-            :nth-child(1){
-            font-size: 6.3vw;        
+    img {
+      width: 50%;
+      object-fit: cover;
+      object-position: 65% 50%;
+      filter: grayscale(1);
+    }
+  }
+}
+
+
+@media (min-width: 1024px) {
+  .container-hero {
+    &__left {
+      &-title {
+        :nth-child(1) {
+          font-size: 6.3vw;
         }
-            :nth-child(2){
-                font-size: 1.5vw;     
-                font-weight: 300;
-               
-            }
+
+        :nth-child(2) {
+          font-size: 1.5vw;
+          font-weight: 300;
+
         }
-        &-buttons{
-           
+      }
+
+      &-buttons {}
+
+      &-social {
+        display: flex;
+        flex-direction: row-reverse;
+        align-items: center;
+        justify-content: flex-end;
+
+        &-line {
+          height: rem(1);
+          width: rem(103);
+          background-color: $gray-soft;
+          margin: 0 rem(20);
         }
-        &-social{
-           display: flex;
-           flex-direction: row-reverse;
-           align-items: center;
-           justify-content: flex-end;
-           &-line{
-            height: rem(1);
-            width: rem(103);
-            background-color: $gray-soft;
-            margin: 0 rem(20);
-           }
-        } 
+      }
     }
   }
 }
@@ -146,38 +173,43 @@ const { data: home, error } = await useAsyncData('home', () => client.getSingle(
 
 
 //Media queries pour adapter l'espacement vertical du hero
-  @media (min-width: 1440px) {
-    .container-hero {
-    &__left{
-        &-title{
+@media (min-width: 1440px) {
+  .container-hero {
+    &__left {
+      &-title {
         padding: rem(0) rem(0) rem(35) rem(60);
-        }
-        &-buttons{
-            padding: rem(0) rem(60) rem(35) rem(60);
-        }
-        &-social{
-          font-size: $size-16;
-         
-            padding: rem(0) rem(60) rem(35) rem(60);
-            &-icons{
-              gap: rem(19);
-              img{
-                width: rem(25);
-                height: rem(25);
-             }
-            }
-        } 
-        &-title{
-            :nth-child(1){
-            font-size: 6.3vw;          
+      }
+
+      &-buttons {
+        padding: rem(0) rem(60) rem(35) rem(60);
+      }
+
+      &-social {
+        font-size: $size-16;
+
+        padding: rem(0) rem(60) rem(35) rem(60);
+
+        &-icons {
+          gap: rem(19);
+
+          img {
+            width: rem(25);
+            height: rem(25);
           }
-          :nth-child(2){
-            font-size: 1.80vw;  
-                letter-spacing: 0.2vw;    
-                white-space: nowrap;
-            }
         }
+      }
+
+      &-title {
+        :nth-child(1) {
+          font-size: 6.3vw;
+        }
+
+        :nth-child(2) {
+          font-size: 1.80vw;
+          letter-spacing: 0.2vw;
+          white-space: nowrap;
+        }
+      }
     }
   }
-} 
-</style>
+}</style>
