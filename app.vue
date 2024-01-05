34e6e10953568c63267979bc804bf4369ebc8072
DavidSark <david.sarkissian@edu.univ-fcomte.fr>
2024-01-05 08:08:28 +0100

    feat(header) : ajout du script pour le changement de couleur du menu en fonction du scroll

--- app.vue
+++ app.vue
@@ -9,29 +9,37 @@
 //   'offset': 0
 // });
 
-// const {LocomotiveScroll} = useNuxtApp()
-// onMounted(()=>{
-//   const scroll = new LocomotiveScroll({
-//     el: document.querySelector('[data-scroll-container]'),
-//     smooth: true,
-//     smartphone: {
-//       smooth: true,
-//     },
-//     tablet: {
-//       smooth: true,
-//     },
-//   });
+const {LocomotiveScroll} = useNuxtApp()
+onMounted(()=>{
+  const scroll = new LocomotiveScroll({
+    el: document.querySelector('[data-scroll-container]'),
+    smooth: true,
+    smartphone: {
+      smooth: true,
+    },
+    tablet: {
+      smooth: true,
+    },
+  });
 
-//   // console.log(scroll);
-//   scroll.on('scroll', (args) => {
-//     const header = document.getElementsByClassName('header')[0];
-//     if (args.scroll.y > 0) {
-//       header.classList.add('-bg-white');
-//     } else {
-//       header.classList.remove('-bg-white');
-//     }
-//   });
-// });
+  // console.log(scroll);
+  scroll.on('scroll', (args) => {
+    const header = document.getElementsByClassName('header')[0];
+    if (args.scroll.y > 0) {
+      header.classList.add('-bg-white');
+    } else {
+      header.classList.remove('-bg-white');
+    }
+
+    const menuIcon = document.getElementsByClassName('header-menu')[0];
+    if(args.scroll.y > 0){
+      menuIcon.classList.add('-colorBlack');
+    }else{
+      menuIcon.classList.remove('-colorBlack');
+    }
+  });
+});
+  
 
 
 
