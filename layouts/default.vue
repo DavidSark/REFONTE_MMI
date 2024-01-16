965d4f755b6e47453410bee9a550291d07cca2f7
DavidSark <david.sarkissian@edu.univ-fcomte.fr>
2024-01-16 05:15:37 +0100

    feat(header) : correctif

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,7 +6,7 @@ const { data: home, error } = await useAsyncData('home', () => client.getSingle(
 
 <template>
     <div>
-        <!-- <Header :elements="home.data.header"></Header> -->
+        <Header :elements="home.data.header"></Header>
             <slot></slot>
         <Footer></Footer>
     </div>
